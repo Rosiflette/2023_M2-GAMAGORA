11f0ec2449723a3a83bc465a81929a61c37ee849
Rose <janinejacques.tod@gmail.com>
2023-11-24 11:54:17 +0100

    NEW : Séance interface TP 2

--- Interface/TP01/TestOpenCV2022.cs
+++ Interface/TP01/TestOpenCV2022.cs
@@ -4,24 +4,115 @@ using Emgu.CV.Structure;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Util;
 using System.Numerics;
+using System.Drawing;
 
 namespace TestOpenCV2022
 {
     internal class TestOpenCV2022
     {
-        public static void CopyToImage(Image<Bgr, Byte> source, Image<Bgr, Byte> destination, int offsetX, int offsetY)
+        public static void CopyToImage(Image<Bgr, Byte> source, Image<Bgr, Byte> destination, int offsetX = 0, int offsetY = 0)
         {
             for(int x = 0; x < source.Width; x++)
             {
+                if(x + offsetX >= destination.Width)
+                {
+                    break;
+                }
                 for (int y = 0; y < source.Height; y++)
                 {
+                    if(y + offsetY >= destination.Height)
+                    {
+                        break;
+                    }
                     destination.Data[y + offsetY, x+offsetX, 0] = source.Data[y, x, 0];
                     destination.Data[y + offsetY, x+offsetX, 1] = source.Data[y, x, 1];
                     destination.Data[y + offsetY, x+offsetX, 2] = source.Data[y, x, 2];
                 }
             }
-            
         }
+
+        public static void CopyToImage(Image<Gray, Byte> source, Image<Bgr, Byte> destination, int offsetX = 0, int offsetY = 0)
+        {
+            for (int x = 0; x < source.Width; x++)
+            {
+                if (x + offsetX >= destination.Width)
+                {
+                    break;
+                }
+                for (int y = 0; y < source.Height; y++)
+                {
+                    if (y + offsetY >= destination.Height)
+                    {
+                        break;
+                    }
+                        destination.Data[y + offsetY, x + offsetX, 0] = (byte)source[y, x].Intensity;
+                        destination.Data[y + offsetY, x + offsetX, 1] = (byte)source[y, x].Intensity;
+                        destination.Data[y + offsetY, x + offsetX, 2] = (byte)source[y, x].Intensity;
+                }
+            }
+        }
+
+        public static void CopyChannelToImage(Image<Bgr, Byte> source, Image<Bgr, Byte> destination, int offsetX, int offsetY, int canal)
+        {
+            for (int x = 0; x < source.Width; x++)
+            {
+                if (x + offsetX >= destination.Width)
+                {
+                    break;
+                }
+                for (int y = 0; y < source.Height; y++)
+                {
+                    if (y + offsetY >= destination.Height)
+                    {
+                        break;
+                    }
+                    destination.Data[y + offsetY, x + offsetX, canal] = (byte)source.Data[y, x, canal];
+                }
+            }
+        }
+
+        public static void CopyChannelToImage(Image<Hsv, Byte> source, Image<Hsv, Byte> destination, int channel, int offsetX = 0, int offsetY = 0)
+        {
+            for (int x = 0; x < source.Width; x++)
+            {
+                if (x + offsetX >= destination.Width)
+                {
+                    break;
+                }
+                for (int y = 0; y < source.Height; y++)
+                {
+                    if (y + offsetY >= destination.Height)
+                    {
+                        break;
+                    }
+                    destination.Data[y + offsetY, x + offsetX, channel] = (byte)source.Data[y, x, channel];
+                }
+            }
+        }
+
+        public static void CopyChannelToImage(Image<Hsv, Byte> source, Image<Gray, Byte> destination, int channel, int offsetX = 0, int offsetY = 0)
+        {
+            for (int x = 0; x < source.Width; x++)
+            {
+                if (x + offsetX >= destination.Width)
+                {
+                    break;
+                }
+                for (int y = 0; y < source.Height; y++)
+                {
+                    if (y + offsetY >= destination.Height)
+                    {
+                        break;
+                    }
+                    destination.Data[y + offsetY, x + offsetX, 0] = (byte)source.Data[y, x, channel];
+                }
+            }
+        }
+
+
+
+
+
         static void Main(string[] args)
         {
 
@@ -49,15 +140,183 @@ namespace TestOpenCV2022
 
 
             // PARTIE 3
-            Image<Bgr, Byte> img = new Image<Bgr, Byte>("C:\\Users\\rchapelle\\Documents\\2023_M2-GAMAGORA\\Interface\\Images\\crochet.jpg");
+            //Image<Bgr, Byte> img = new Image<Bgr, Byte>("C:\\Users\\rchapelle\\Documents\\2023_M2-GAMAGORA\\Interface\\Images\\crochet.jpg");
+            //Image<Bgr, Byte> img2 = new Image<Bgr, Byte>("C:\\Users\\rchapelle\\Documents\\2023_M2-GAMAGORA\\Interface\\Images\\crochet.jpg");
+            //int height;
+            //if (img.Height > img2.Height)
+            //{
+            //    height = img.Height;
 
-            Image<Bgr, Byte> img2 = new Image<Bgr, Byte>(1000,1000);
+            //}
+            //else
+            //{
+            //    height = img2.Height;
+            //}
+            //Image<Bgr, Byte> img3 = new Image<Bgr, Byte>(img.Width + img2.Width,height);
+            //Image<Bgr, Byte> img3 = new Image<Bgr, Byte>(img.Width * 2, img.Height);
 
 
-            CopyToImage(img, img2, 0,0);
+            //CopyChannelToImage(img, img2, 0,0, 0);
 
 
-            CvInvoke.Imshow("Exercice 2", img2);
+            // PARTIE 4
+
+            //CvInvoke.CvtColor(img2, img2, ColorConversion.Bgr2Gray);
+            //CvInvoke.Flip(img2, img2, FlipType.Vertical);
+
+            //CopyToImage(img, img3, 0, 0);
+            //CopyToImage(img2, img3, img.Width, 0);
+            //CvInvoke.Imshow("Exercice 1", img3);
+            //CvInvoke.WaitKey(0);
+
+
+            // PARTIE 5
+            //Image<Hsv, byte> img1 = new Image<Hsv, byte>("C:\\Users\\rchapelle\\Documents\\2023_M2-GAMAGORA\\Interface\\Images\\crochet.jpg");
+            //Image<Gray, byte> img2 = new Image<Gray, byte>(img1.Width * 3, img1.Height);
+
+
+            //int teinteMin = 30;
+            //int saturationMin = 0;
+            //int valeurMin = 0;
+            //Hsv borneInf = new Hsv(teinteMin, saturationMin, valeurMin);
+            //int teinteMax = 80;
+            //int saturationMax = 255;
+            //int valeurMax = 255;
+            //Hsv borneMax = new Hsv(teinteMax, saturationMax, valeurMax);
+
+            //Image<Gray, byte> img3 = img1.InRange(borneInf, borneMax) ;
+            //CopyChannelToImage(img1, img2, 0);
+            //CopyChannelToImage(img1, img2, 1, img1.Width);
+            //CopyChannelToImage(img1, img2, 2, img1.Width * 2);
+            //CvInvoke.Imshow("channel", img3);
+            //CvInvoke.WaitKey(0);
+
+
+            // PARTIE 6
+            //Image<Hsv, byte> img1 = new Image<Hsv, byte>("C:\\Users\\rchapelle\\Documents\\2023_M2-GAMAGORA\\Interface\\Images\\crochet.jpg");
+            //Image<Gray, byte> img2 = new Image<Gray, byte>(img1.Width * 3, img1.Height);
+
+
+            //int teinteMin = 30;
+            //int saturationMin = 0;
+            //int valeurMin = 0;
+            //Hsv borneInf = new Hsv(teinteMin, saturationMin, valeurMin);
+            //int teinteMax = 80;
+            //int saturationMax = 255;
+            //int valeurMax = 255;
+            //Hsv borneMax = new Hsv(teinteMax, saturationMax, valeurMax);
+
+            //Image<Gray, byte> img3 = img1.InRange(borneInf, borneMax);
+            //CvInvoke.Erode(img3, img3, null, new System.Drawing.Point(-1, -1), 10, BorderType.Default, default);
+            //CvInvoke.Dilate(img3, img3, null, new System.Drawing.Point(-1, -1), 10, BorderType.Default, default);
+
+            //CvInvoke.Imshow("channel", img3);
+            //CvInvoke.WaitKey(0);
+
+            // PARTIE 7
+
+            //Image<Hsv, byte> img1 = new Image<Hsv, byte>("C:\\Users\\rchapelle\\Documents\\2023_M2-GAMAGORA\\Interface\\Images\\crochet.jpg");
+            //Image<Gray, byte> img2 = new Image<Gray, byte>(img1.Width * 3, img1.Height);
+            //Image<Bgr, byte> imgBlack = new Image<Bgr, byte>(1000,1000);
+
+
+            //int teinteMin = 30;
+            //int saturationMin = 0;
+            //int valeurMin = 0;
+            //Hsv borneInf = new Hsv(teinteMin, saturationMin, valeurMin);
+            //int teinteMax = 80;
+            //int saturationMax = 255;
+            //int valeurMax = 255;
+            //Hsv borneMax = new Hsv(teinteMax, saturationMax, valeurMax);
+
+            //Image<Gray, byte> img3 = img1.InRange(borneInf, borneMax);
+
+            //CvInvoke.Erode(img3, img3, null, new System.Drawing.Point(-1, -1), 10, BorderType.Default, default);
+            //CvInvoke.Dilate(img3, img3, null, new System.Drawing.Point(-1, -1), 10, BorderType.Default, default);
+
+
+            //VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            //CvInvoke.FindContours(img3, contours, new Mat(), RetrType.List, ChainApproxMethod.ChainApproxNone);
+
+            //if(contours.Size > 0)
+            //{
+            //    VectorOfPoint largestContour = contours[0];
+            //    for(int i = 0; i < contours.Size; i++)
+            //    {
+
+            //        if(CvInvoke.ContourArea(largestContour) > CvInvoke.ContourArea(contours[i]))
+            //        {
+            //            largestContour = contours[i];
+            //        }
+            //    }
+
+            //    Image<Gray, byte> imgContour = new Image<Gray, byte>(img3.Width, img3.Height);
+            //    imgContour.DrawPolyline(largestContour.ToArray(), false, new Gray(255));
+            //    CopyToImage(imgContour, imgBlack);
+            //    Moments m = CvInvoke.Moments(largestContour);
+            //    Point center = new Point(
+            //        (int)(m.M10 / m.M00),
+            //        (int)(m.M01 / m.M00));
+            //    Image<Bgr, byte> momentImage = imgContour.Convert<Bgr, byte>();
+            //    CvInvoke.Circle(momentImage, center, 5, new MCvScalar(255, 255, 0));
+            //    CopyToImage( momentImage, imgBlack);
+
+            //}
+
+            //CvInvoke.Imshow("channel", imgBlack);
+            //CvInvoke.WaitKey(0);
+
+            // PARTIE 8
+
+            Image<Hsv, byte> img1 = new Image<Hsv, byte>("C:\\Users\\rchapelle\\Documents\\2023_M2-GAMAGORA\\Interface\\Images\\crochet.jpg");
+            Image<Gray, byte> img2 = new Image<Gray, byte>(img1.Width * 3, img1.Height);
+            Image<Bgr, byte> imgBlack = new Image<Bgr, byte>(1000, 1000);
+
+
+            int teinteMin = 30;
+            int saturationMin = 0;
+            int valeurMin = 0;
+            Hsv borneInf = new Hsv(teinteMin, saturationMin, valeurMin);
+            int teinteMax = 80;
+            int saturationMax = 255;
+            int valeurMax = 255;
+            Hsv borneMax = new Hsv(teinteMax, saturationMax, valeurMax);
+
+            Image<Gray, byte> img3 = img1.InRange(borneInf, borneMax);
+
+            CvInvoke.Erode(img3, img3, null, new System.Drawing.Point(-1, -1), 10, BorderType.Default, default);
+            CvInvoke.Dilate(img3, img3, null, new System.Drawing.Point(-1, -1), 10, BorderType.Default, default);
+
+
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(img3, contours, new Mat(), RetrType.List, ChainApproxMethod.ChainApproxNone);
+
+            if (contours.Size > 0)
+            {
+                VectorOfPoint largestContour = contours[0];
+                for (int i = 0; i < contours.Size; i++)
+                {
+
+                    if (CvInvoke.ContourArea(largestContour) > CvInvoke.ContourArea(contours[i]))
+                    {
+                        largestContour = contours[i];
+                    }
+                }
+
+                Image<Gray, byte> imgContour = new Image<Gray, byte>(img3.Width, img3.Height);
+                imgContour.DrawPolyline(largestContour.ToArray(), false, new Gray(255));
+                CopyToImage(imgContour, imgBlack);
+                Moments m = CvInvoke.Moments(largestContour);
+                Point center = new Point(
+                    (int)(m.M10 / m.M00),
+                    (int)(m.M01 / m.M00));
+                Image<Bgr, byte> momentImage = imgContour.Convert<Bgr, byte>();
+                CvInvoke.Circle(momentImage, center, 5, new MCvScalar(255, 255, 0));
+                CopyToImage(momentImage, imgBlack);
+
+            }
+
+            CvInvoke.Imshow("channel", imgBlack);
             CvInvoke.WaitKey(0);
 
         }
@@ -65,24 +324,3 @@ namespace TestOpenCV2022
 
 
 }
-
-
-//string name = "crochet";
-//var img = new Mat($"C:\\Users\\ymiollany\\Pictures\\{name}.png");
-
-//Image<Bgr, Byte> imageCopy = img.ToImage<Bgr, Byte>();
-
-//String win1 = "Test Window (Press any key to close)"; //The name of the window
-//CvInvoke.NamedWindow(win1); //Create the window using the specific name
-//using (Mat frame = new Mat())
-//using (VideoCapture capture = new VideoCapture())
-//    while (CvInvoke.WaitKey(1) == -1)
-//    {
-//        capture.Read(frame);
-
-//        Image<Bgr, Byte> frameImage = frame.ToImage<Bgr, Byte>();
-
-//        CopyToImage(imageCopy, frameImage, new Vector2(10, 10));
-
-//        CvInvoke.Imshow(win1, frameImage);
-//    }
