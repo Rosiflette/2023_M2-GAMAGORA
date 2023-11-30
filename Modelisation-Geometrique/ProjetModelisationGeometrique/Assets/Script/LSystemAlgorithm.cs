914e6aea9e76cf8c94e461164664bf40cda3011d
Rose <janinejacques.tod@gmail.com>
2023-11-30 17:33:17 +0100

    NEW : Projet modélisation géométrique

---
+++ Modelisation-Geometrique/ProjetModelisationGeometrique/Assets/Script/LSystemAlgorithm.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LSystemAlgorithm : MonoBehaviour
+{
+
+    [SerializeField] string rule;
+    [SerializeField] char axiom;
+    [SerializeField] int depth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
